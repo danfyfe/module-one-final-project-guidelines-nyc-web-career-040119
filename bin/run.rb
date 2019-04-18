d3ceffb4b89822848a48dae451666785bea76e03
danfyfe <fyfedaniel@gmail.com>
2019-04-18 14:51:49 -0400

    more squigglies and invalid_command_prompts

--- bin/run.rb
+++ bin/run.rb
@@ -12,6 +12,7 @@ is_returning_user?(input)
 Current_user = User.all.find_by(name:input)
 what_would_you_like_to_do_main
 input = get_user_input
+puts "~" * 22
 i_want_to_do_this(input,Current_user)
 # input = get_user_input
 # i_want_to_search_this_by(input)
